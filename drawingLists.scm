ad93574e53fec9b5e5a4e93cd2831e21f403aef4
VoidWalker <isaacouriques01@gmail.com>
2025-02-03 15:25:27 -0500

    First and second exercise done.

--- drawingLists.scm
+++ drawingLists.scm
@@ -5,3 +5,9 @@
     '()
     (cons (x (car ls))
           (accumulate2 x (cdr ls)))))
+
+(define (accumulate3 x y l1 l2)
+(if (or (null? l1) (null? l2))
+    '()
+    (cons (list (x (car l1)) (y (car l2)))
+        (accumulate3 x y (cdr l1) (cdr l2)))))
