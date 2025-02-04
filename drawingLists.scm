3b79aec19fcb4ab9f1e2b70042cf41eaa94f6bd7
VoidWalker02 <isaacouriques01@gmail.com>
2025-02-03 19:58:16 -0500

    Done.

--- drawingLists.scm
+++ drawingLists.scm
@@ -1,5 +1,4 @@
 
-
 (define (accumulate2 x ls)
 (if (null? ls)
     '()
@@ -11,3 +10,16 @@
     '()
     (cons (list (x (car l1)) (y (car l2)))
         (accumulate3 x y (cdr l1) (cdr l2)))))
+
+(define (bridges distance sorted-distances)
+(cond
+
+    [(= distance 0) #t]
+
+    [(or (null? sorted-distances) (< distance 0)) #f]
+
+    [else
+        (or
+            (bridges (- distance (car sorted-distances)) sorted-distances)
+
+            (bridges distance (cdr sorted-distances)))]))
